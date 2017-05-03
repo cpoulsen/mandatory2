8d3315606b542db2863efe4804ccd9c97df94280
mmchr <martin.m.chr@gmail.com>
2017-05-03 12:05:21 +0200

    Minor change

--- client/src/app/chatroom.service.obsolete.ts
+++ client/src/app/chatroom.service.obsolete.ts
@@ -2,57 +2,25 @@ import { Injectable }     from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable }     from 'rxjs/Observable';
 import 'rxjs/Rx';
-import { Chat } from "./chat/chat.model";
 import { Chatroom } from "./chatroom/chatroom.model";
 import * as io from 'socket.io-client';
 
 @Injectable()
-export class ChatService {
-    private getChatUrl = 'message/get';  // URL to web API
-    private postChatUrl = 'message/post';  // URL to web API
+export class ChatroomService {
     private getChatroomsUrl = 'chatroom/get';  // URL to web API
     private postChatroomUrl = 'chatroom/post';  // URL to web API
     constructor (private http: Http) {}
+    private socket;
     private url = window.location.origin;
-    private socket = io(this.url);
-
-
-
-    getChatMessagesFromServer(selectedChatRoom): Observable<Chat[]> {
-        let observable = new Observable(observer => {
-            console.log("Socket:",this.url + selectedChatRoom);
-            this.socket.on('refreshMessages', (data) => {
-                observer.next(data);
-            });
-
-            return () => {
-                this.socket.disconnect();
-            };
-        });
-        return observable;
-    }
 
     /*
-    getChatMessagesFromServer(selectedChatRoom): Observable<Chat[]> {
-        this.getChatUrl = 'message/get/'+selectedChatRoom
-        return this.http.get(this.getChatUrl)
-            .map(this.extractData)
-            .catch(this.handleError);
-    }
-    */
-
-    addChatMessage(chat: Chat): Observable<Chat> {
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
-
-        return this.http.post(this.postChatUrl, chat, options)
-            .map(this.extractData)
-            .catch(this.handleError);
-    }
+     * Get blog messages from server
+     */
 
     getChatroomsFromServer(): Observable<Chatroom[]> {
         let observable = new Observable(observer => {
             console.log("Socket:",this.url);
+            this.socket = io(this.url);
             this.socket.on('refreshChat', (data) => {
                 observer.next(data);
             });
@@ -63,7 +31,7 @@ export class ChatService {
         });
         return observable;
     }
-
+    
 
     addChatroom(chatroom: Chatroom): Observable<Chatroom> {
         let headers = new Headers({ 'Content-Type': 'application/json' });
